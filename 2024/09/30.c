f59a04b87a62766d88c54700aee56e9c5793353f
stivenbushaj <bushajstiven2@gmail.com>
2024-09-30 11:45:07 +0000

    maxminuguale

---
+++ 2024/09/30.c
@@ -0,0 +1,44 @@
+/*DATI DUE NUMERI A e B VERIFICARE 
+SE A è il quadrato di B */
+
+#include <stdio.h>
+int main()
+{
+    float A=0;
+    float B=0;
+    float quadrato=0;
+    quadrato=B*B;
+    printf("inserisci il primo valore");
+    scanf("%f",&A);
+    printf("inserisci il primo valore");
+    scanf("%f",&B);
+
+    quadrato = B*B;
+    printf("il doppio è: %f" , quadrato)
+
+
+    if=(A== quadrato){
+    printf("il primo valore %f corrisponde al quadrato del secondo %f",A,B);
+    }
+    else{
+        printf("il primo valore %f non corrisponde al quadrato del secondo %f",A ,B);
+        
+
+    
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
